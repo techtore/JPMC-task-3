da0a0dbfc458a1972f3c641028e948ac3fe2ad29
Victoria Hale <vic.hale74@gmail.com>
2020-06-17 11:02:57 -0700

    update generate function to compute and return properly

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -12,13 +12,24 @@ export interface Row {
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]): Row[] {
-    return serverResponds.map((el: any) => {
+  static generateRow(serverRespond: ServerRespond[]): Row {
+    const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+    const ratio = priceABC / priceDEF;
+    //+/- 10% of 12 month historical average
+    const upperBound = 1 + 0.10;
+    const lowerBound = 1 - 0.10;
+    
+    //return a single row object rather than array of row objects
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+       price_abc: priceABC,
+       price_def: priceDEF,
+       ratio,
+       timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ? 
+        serverRespond[0].timestamp : serverRespond[1].timestamp,
+        upper_bound: upperBound,
+        lower_bound: lowerBound,
+        trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined, 
       };
-    })
   }
 }
