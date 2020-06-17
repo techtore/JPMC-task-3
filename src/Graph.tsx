992bc506b554dd5a345e52d8e1d1964ef4606558
Victoria Hale <vic.hale74@gmail.com>
2020-06-17 10:30:46 -0700

    update elements

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -39,9 +39,8 @@ class Graph extends Component<IProps, {}> {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
         price_abc: 'avg',
         price_def: 'avg',
